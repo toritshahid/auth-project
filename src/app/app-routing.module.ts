bf0be18351ee4b514e4ea6013c32783ff7b92769
Shahid Shabir <shahid.shabir055@gmail.com>
2022-02-20 16:27:10 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OptComponent } from './opt/opt.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'otp', component: OptComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SocialAuthComponent } from './social-auth/social-auth.component';
+import { OptComponent } from './opt/opt.component';
+import { NgOtpInputModule } from  'ng-otp-input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    SocialAuthComponent,
+    OptComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOtpInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
